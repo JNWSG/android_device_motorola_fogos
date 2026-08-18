6b8268bc7dce994a8e01828d6af9d8e42571334f
ᴊɪɴᴡᴏᴏ | ジンウ <jinwoosung@zohomail.in>
2026-08-18 21:12:32 +0530

    fogos: vendorsetup: Update source repository references

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -35,9 +35,9 @@ clone_if_missing "https://github.com/JNWSG/android_device_motorola_sm6375-common
 clone_if_missing "https://github.com/JNWSG/android_vendor_motorola_fogos.git" "16" "vendor/motorola/fogos"
 clone_if_missing "https://github.com/JNWSG/android_vendor_motorola_sm6375-common.git" "16" "vendor/motorola/sm6375-common"
 # Kernel
-clone_if_missing "https://github.com/fogos4ever/android_kernel_motorola_sm6375.git" "kage" "kernel/motorola/sm6375"
+clone_if_missing "https://github.com/FogosLab/android_kernel_motorola_sm6375.git" "kage" "kernel/motorola/sm6375"
 # Hardware
-clone_if_missing "https://github.com/fogos4ever/android_hardware_motorola.git" "lineage-23.2" "hardware/motorola"
+clone_if_missing "https://github.com/FogosLab/android_hardware_motorola.git" "lineage-23.2" "hardware/motorola"
 clone_if_missing "https://github.com/JNWSG/hardware_dolby.git" "16.0" "hardware/dolby"
 
 echo "--- Source Sync Complete ---"
