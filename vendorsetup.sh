9c726b3e2b05a60c6e8cf9b2fdc98961e9a2b9e9
ᴊɪɴᴡᴏᴏ | ジンウ <jinwoosung@zohomail.in>
2026-08-19 10:53:46 +0530

    fogos:  vendorsetup: update repository branch for version 17

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -29,11 +29,11 @@ clone_if_missing() {
 }
 
 # Device
-clone_if_missing "https://github.com/JNWSG/android_device_motorola_fogos.git" "16" "device/motorola/fogos"
-clone_if_missing "https://github.com/JNWSG/android_device_motorola_sm6375-common.git" "16" "device/motorola/sm6375-common"
+clone_if_missing "https://github.com/JNWSG/android_device_motorola_fogos.git" "17" "device/motorola/fogos"
+clone_if_missing "https://github.com/JNWSG/android_device_motorola_sm6375-common.git" "17" "device/motorola/sm6375-common"
 # Vendor
-clone_if_missing "https://github.com/JNWSG/android_vendor_motorola_fogos.git" "16" "vendor/motorola/fogos"
-clone_if_missing "https://github.com/JNWSG/android_vendor_motorola_sm6375-common.git" "16" "vendor/motorola/sm6375-common"
+clone_if_missing "https://github.com/JNWSG/android_vendor_motorola_fogos.git" "17" "vendor/motorola/fogos"
+clone_if_missing "https://github.com/JNWSG/android_vendor_motorola_sm6375-common.git" "17" "vendor/motorola/sm6375-common"
 # Kernel
 clone_if_missing "https://github.com/FogosLab/android_kernel_motorola_sm6375.git" "kage" "kernel/motorola/sm6375"
 # Hardware
